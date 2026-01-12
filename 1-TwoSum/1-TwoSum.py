1eda2093248da24e1d5da799f69ead5cc86acf40
Blake Mills <113030339+Blaker-Space@users.noreply.github.com>
2026-01-12 12:58:29 -0600

    File updated at 1/12/2026, 12:58:29 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,24 +1,12 @@
-# Last updated: 12/29/2025, 2:28:08 PM
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        # define a map to hold index/value pairs
-        valueToIndex = {}
-
-        # iterate over all numbers in nums once (O(n)), taking the current index and value into consideration
-        for i, num in enumerate(nums):
-
-            # num + some value x = target iff some value x = target - num
-            # check if target - num (the value that would make num + some value x = target) is a value in our map...
-            if target - num in valueToIndex:
-
-                # if it is, we know that value target - num + num = target. return index of target-num and num:
-                return [valueToIndex[target-num], i]
-            
-            # add the current value as a key to our map, set its value equal to the index at i
-            valueToIndex[num] = i
-        
+# Last updated: 1/12/2026, 12:58:29 PM
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        
+4        #define a dictionary for holding indices and values
+5        valueDict = {}
+6
+7        # iterate over all nums
+8        for i, num in enumerate(nums):
+9            if target - num in valueDict:
+10                return[valueDict[target-num], i]
+11            valueDict[num] = i
